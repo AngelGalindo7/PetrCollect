04879a4db89745471957a0d0d3c1919d9d529772
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2026-02-01 12:21:04 -0800

    feat(backend/main): Adding mount a seperate file serving app to retrieve uploaded images

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 from .database import get_db
 from .routers import auth, users, posts
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 #. relative import current package .. import from parent package
 #packages make relative imports reliable
 
@@ -23,6 +24,7 @@ app.add_middleware(
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(posts.router)
+app.mount("/uploads", StaticFiles(directory="Uploads"), name="uploads")
 @app.get("/test-db/")
 def test_db(db: Session = Depends(get_db)):
     try:
