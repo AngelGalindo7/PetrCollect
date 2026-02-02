9317a148e156caae87cb16e1eeede78c3d847f2a
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2026-02-02 13:49:08 -0800

    feat(frontend/App): Added dynamic routing for user profiles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import SignUp from './pages/SignUp'
-import LogIn from './pages/login'
+//import SignUp from './pages/SignUp'
+import LogIn from './pages/LogIn'
+import UserProfile from './pages/UserProfile'
+import HomePage from './pages/HomePage'
 function App(){
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<LogIn />} />
+      <Route path="/:username" element={<UserProfile />} />
+      <Route path="/Home" element={<HomePage />} />
     </Routes>
     </BrowserRouter>
   )
