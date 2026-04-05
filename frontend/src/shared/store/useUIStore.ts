75adae31ed64edfd540842113619848e6e3d80df
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2026-04-05 14:22:59 -0700

    feat(frontend store): add isCreateMenuOpen state to useUIStore

--- frontend/src/shared/store/useUIStore.ts
+++ frontend/src/shared/store/useUIStore.ts
@@ -1,13 +1,19 @@
 import { create } from 'zustand';
 
 interface UIState {
+  isCreateMenuOpen: boolean;
+  openCreateMenu: () => void;
+  closeCreateMenu: () => void;
   isCreatePostModalOpen: boolean;
   openCreatePostModal: () => void;
   closeCreatePostModal: () => void;
 }
 
 export const useUIStore = create<UIState>((set) => ({
+  isCreateMenuOpen: false,
+  openCreateMenu: () => set({ isCreateMenuOpen: true }),
+  closeCreateMenu: () => set({ isCreateMenuOpen: false }),
   isCreatePostModalOpen: false,
-  openCreatePostModal: () => set({ isCreatePostModalOpen: true }),
+  openCreatePostModal: () => set({ isCreatePostModalOpen: true, isCreateMenuOpen: false }),
   closeCreatePostModal: () => set({ isCreatePostModalOpen: false }),
 }));
