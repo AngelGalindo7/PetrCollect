607e3d5916462c8be633da8694405bc19c073a1f
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2026-04-04 21:16:41 -0700

    feat(backend routers): register folders router in main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from .database import get_db
-from .routers import auth, users, posts
+from .routers import auth, users, posts, folders
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 #. relative import current package .. import from parent package
@@ -24,6 +24,7 @@ app.add_middleware(
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(posts.router)
+app.include_router(folders.router)
 app.mount("/Uploads", StaticFiles(directory="Uploads"), name="uploads")
 @app.get("/test-db/")
 def test_db(db: Session = Depends(get_db)):
